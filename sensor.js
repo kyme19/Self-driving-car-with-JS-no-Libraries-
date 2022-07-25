786e10d14a2a99e2112ae867040bcbf7962766ea
kyme19 <58340050+kyme19@users.noreply.github.com>
2022-07-25 13:51:49 +0300

    Create sensor.js

---
+++ sensor.js
@@ -0,0 +1,85 @@
+class sensor {
+    constructor (car){
+        this.car =car;
+        this.rayCount = 5;
+        this.rayLenght = 100;
+        this.raySpread = math.PI/4;
+
+        this.rays = [];
+        this.readings=[];
+    }
+
+    update(roadBorders){
+        this.#castRays();
+        this.readings =[];
+        for (let i = 0; i< this.rays.length; i++){
+         this.readings.push(
+             this.#getReading(this.rays[i],roadBorders)
+         );
+        }
+    }
+#getReading(ray,roadBorders){
+    let touches=[];
+
+    for(let i =0; i<roadBorders.length; i++){
+        const touch=getIntersection(
+            ray[0],
+            ray[1],
+            roadBorders[i][0],
+            roadBorders[i][1]
+        );   
+        if(touch){
+            touches.push(touch);
+        }
+ }
+      if(touches.length==0){
+          return null;
+      } else{
+          const offsets = touches.map(e=>e.offset);
+          const minOffset = Math.min(...offsets);
+          return touches.find (e=>e.offset==minOffset);
+      }
+}
+
+#castRays(){
+    
+    this.rays=[];
+    for(let i=o; i<this.rayCount; i++){
+        const rayAngle = lerp(
+            this,rayspread/2,
+            -this.raySpread/2,
+            this.rayCount == 1?.5 : i/this.rayCount
+        )+this.car.angle;
+
+        const start ={x:start.car.x , y:this.car.y};
+        const end = {
+             x:this.car.x - Math.sin(rayAngle)*this.rayLenght,
+             y:this.car.y - Math.cos(rayAngle)*this.rayLenght
+        };
+        this.rays.push(new ray(start,end));
+}
+
+}
+     draw(ctx){
+         for(let i =0; i<this.rayCount; i++){
+             let end=this.rays[i][1];
+             if(this.readings[i]){
+                 end=this.readings[i];
+             }
+             ctx.beginPath();
+             ctx.lineWidth=2;
+             ctx.strokeStyle="black";
+
+             ctx.moveTo(
+                        this.rays[i][1].x,
+                        this.rays[i][1].y
+                        );
+            ctx.lineTo(
+              end.x,
+              end.y
+          );
+            ctx.stroke();
+        }
+      } 
+    }
+
