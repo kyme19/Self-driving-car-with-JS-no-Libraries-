2eb08a414f52362ad9a1f8eee087e135991daf7d
kyme19 <58340050+kyme19@users.noreply.github.com>
2022-08-06 04:26:59 -0700

    Update main.js
    // added traffic for incoming dummy cars using a new method traffic in this case..

--- main.js
+++ main.js
@@ -10,15 +10,26 @@ const canvas= document.getElementById('myCanvas');
 canvas.width=200;
 const ctx= canvas.getContext("2d");
 const road= new road(canvas.width/2,canvas.width*0.9);
-const car = Car(road.getLaneCenter(1),100,30,50);
-
+const car = Car(road.getLaneCenter(1),100,30,50,"KEYS");
+//lets add traffic using arrays
+const traffic=[
+    new Car (road.getLaneCenter(1),-100,30,50,"DUMMY")
+];
 animate();
 function animate(){
+    
+    for(let i=0; i<traffic.length; i++){
+        traffic[1].update(road.borders);
+    }
+    
     car.update(road.borders);
     canvas.height=window.innerHeight;
     ctx.traslate(0,-car.y+canvas.height*0.7);
 
     road.draw(ctx);
+    for (let i=0; i<traffic.length; i++){
+        traffic[i].draw(ctx);
+    }
     car.draw(ctx);
 
     ctx.restore();
