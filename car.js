9e8ad628dab8c0af45b04e94feef483b93bfda28
kyme19 <58340050+kyme19@users.noreply.github.com>
2022-08-06 03:53:30 -0700

    Update car.js
    slight changes on polygons which give the car shapes and also for intersecting with the road borders upon contact to register the assessed damage inform of points (a method created  for that )

--- car.js
+++ car.js
@@ -10,15 +10,59 @@ class Car{
         this.maxSpeed=3;
         this.friction=0.05;
         this.angle=0;
-
+        //continue
+        this.damaged=false;
+        
+        
 
         this.sensor =new this.sensor(this);
         this.controls=new this.controls();
 
     }   update(roadBorders){
       this.move();
+      this.polygon=this.#createPolygon();
+      this.damaged=this.#assessDamage(roadBorders); 
       this.sensor.update(roadBorders);
     }
+    
+    #assessDamage(roadBorders)
+    {
+        for( let i=0, i<roadBorders, i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    
+    
+    
+    #createPolygon(){
+         const point = [];
+         const rad=Math.hypot(this.width,this.height)/2;
+         const alpha=Math.atan2(this.width,this.height);
+         points.push(
+             x:this.x-Math.sin(this.angle-alpha)*rad,
+             y:this.y-Math.sin(this.angle-alpha)*rad
+    });
+         points.push(
+             x:this.x-Math.sin(this.angle+alpha)*rad,
+             y:this.y-Math.sin(this.angle+alpha)*rad
+    });
+         points.push(
+             x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+             y:this.y-Math.sin(Math.PI+this.single-alpha)*rad
+    });
+         points.push(
+             x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+             y:this.y-Math.sin(Math.PI+this.single+alpha)*rad
+    });
+         return points;
+}
+    
+    
 #move(){
     if(this.controls.foward){
         this.speed+=this.acceleration;
@@ -66,7 +110,16 @@ if(this.controls.right){
 
 
     draw(ctx){
-        ctx.save();
+        
+        ctx.beginpath();
+        ctx.moveTo(this.polygon[0].x,this.polygon[0]-y);
+        for (let i=1; i<this.polygon.length,i++){
+              ctx.lineTo(this.polygon[1].x,this.polygon[1]-y);
+        }
+            ctx.fill();
+            
+        
+        /* ctx.save();
         ctx.translate(this.x,this.y);//translate the car to the center of the canvas
         ctx.rotate(-this.angle);//rotate the car according to the angle
        
@@ -80,7 +133,8 @@ if(this.controls.right){
                 ctx.fill();
  
 
-                ctx.restore();
+                ctx.restore(); 
+        */
 
                 this.sensor.draw(ctx);
   }
