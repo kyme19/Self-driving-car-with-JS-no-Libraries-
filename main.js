f7477615c1d5300e3722b115820244ca6f6a1204
kyme <58340050+kyme19@users.noreply.github.com>
2022-08-08 05:34:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const networkCtx= networkCanvas.getContext("2d");
 
 const road= new road(carCanvas.width/2,carCanvas.width*0.9);
 
-const N=100;
+const N=1;
 const cars=generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
@@ -24,7 +24,7 @@ if(localStorage.getItem("bestBrain")){
     cars[i].brain=JSON.parse(
          localStorage.getItem("bestBrain"));
         if(i!=0){
-            NeuralNetwork.mutate(cars[i].brain,0.2);
+            NeuralNetwork.mutate(cars[i].brain,0.1);
         }
     }
 }
@@ -34,7 +34,10 @@ const traffic=[
     new Car (road.getLaneCenter(1),-100,30,50,"DUMMY,2"),
     new Car (road.getLaneCenter(0),-300,30,50,"DUMMY,2"),
     new Car (road.getLaneCenter(2),-300,30,50,"DUMMY,2"),
-    
+    new Car (road.getLaneCenter(0),-500,30,50,"DUMMY,2"),
+    new Car (road.getLaneCenter(1),-600,30,50,"DUMMY,2"),
+    new Car (road.getLaneCenter(1),-500,30,50,"DUMMY,2"),
+    new Car (road.getLaneCenter(2),-700,30,50,"DUMMY,2"),
 ];
 animate();
 
